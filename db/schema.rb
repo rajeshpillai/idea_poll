c77f8c944c9e3100bb7e39778994b335474596a1
rajeshpillai <pillai.rajesh@gmail.com>
2022-01-25 10:08:26 +0530

    inbox validations

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,7 @@ ActiveRecord::Schema.define(version: 2022_01_25_043052) do
   enable_extension "plpgsql"
 
   create_table "inboxes", force: :cascade do |t|
-    t.string "name"
+    t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
