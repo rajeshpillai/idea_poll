440725ac3dafd8d26f35aa94aa20ebdddd0792fe
rajeshpillai <pillai.rajesh@gmail.com>
2022-01-26 08:11:57 +0530

    Run seed only in dev env

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,7 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
+return unless Rails.env.development?
 
 puts("Seeding data...")
 
